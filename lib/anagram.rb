79fb6fd674aabfcedb2418554a4207dbff73acbb
danielpdaniel <artofjquinn@gmail.com>
2022-12-22 19:59:19 -0500

    commit tree 38e52b569f6e59a4aa77d762b703913f2d79c6a0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+require "pry"
+
+ class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match array
+        output = []
+        array.each do |anagram|
+           if anagram.chars.sort == word.chars.sort
+            output << anagram
+           end
+        end
+        return output
+    end
+ end
